4d62557d90edc79c3860e18c5f82626c7d4ae1dd
dmi3y <lapshukov@gmail.com>
2016-04-11 12:31:15 -0700

    factory > provider + configs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,9 @@
 /* global angular */
 angular.module('ChatterApp', [])
+.config(function (UserServiceProvider) {
+  UserServiceProvider.setApiEndpoints({
+    apiUri: 'https://chatter-search-api.herokuapp.com',
+    userShow: '/1.2.0/user_show',
+    userTimeline: '/1.2.0/user_timeline'
+  })
+})
--- src/scripts/services/UserService.js
+++ src/scripts/services/UserService.js
@@ -1,24 +1,32 @@
 /* global angular */
 angular.module('ChatterApp')
-.factory('UserService', function UserServiceFactory ($http, $q) {
+.provider('UserService', function UserServiceProvider () {
   'use strict'
-  var apiUri = 'https://chatter-search-api.herokuapp.com'
-  var userShowUri = apiUri + '/1.2.0/user_show'
-  var userTimelineUri = apiUri + '/1.2.0/user_timeline'
+  var apiUri
+  var userShowUri
+  var userTimelineUri
 
-  return {
-    all: function (params) {
-      var user = $http.get(userShowUri,
-        {
-          params: params
-        }
-      )
-      var tweets = $http.get(userTimelineUri,
-        {
-          params: params
-        }
-      )
-      return $q.all([user, tweets])
+  this.setApiEndpoints = function (conf) {
+    apiUri = conf.apiUri
+    userShowUri = apiUri + conf.userShow
+    userTimelineUri = apiUri + conf.userTimeline
+  }
+
+  this.$get = function ($http, $q) {
+    return {
+      all: function (params) {
+        var user = $http.get(userShowUri,
+          {
+            params: params
+          }
+        )
+        var tweets = $http.get(userTimelineUri,
+          {
+            params: params
+          }
+        )
+        return $q.all([user, tweets])
+      }
     }
   }
 })
