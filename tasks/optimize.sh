4555c3b2376068c6d6ebd19a7976388bd49afc29
dmi3y <lapshukov@gmail.com>
2016-04-13 12:24:32 -0700

    ng annotate

--- tasks/optimize.sh
+++ tasks/optimize.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash -x
 
 npm run compile-styles -- -u cssnano & \
-./tasks/compile-scripts.sh | uglifyjs -o ./build/index.js & \
-./tasks/compile-components.sh | uglifyjs -o ./build/lib.js & \
+./tasks/compile-scripts.sh | ng-annotate -a - | uglifyjs -o ./build/index.js & \
+./tasks/compile-components.sh | ng-annotate -a - | uglifyjs -o ./build/lib.js & \
 npm run compile-templates &
 
 wait
