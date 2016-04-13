22ce1f3d66ab92f420e5246cc58d979cf82d843f
dmi3y <lapshukov@gmail.com>
2016-04-13 14:19:32 -0700

    tweaking the scripts

--- tasks/compile-scripts.sh
+++ tasks/compile-scripts.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p ./build
-compress=$1
 
 babel ./src/scripts --source-maps |\
 printf ";(function(){ $(cat -)\n})();"
--- tasks/optimize.sh
+++ tasks/optimize.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env bash -x
 
 npm run compile-styles -- -u cssnano & \
-./tasks/compile-components.sh | uglifyjs -o ./build/lib.js & \
-./tasks/compile-scripts.sh | ng-annotate -a - | uglifyjs -o ./build/index.js & \
+bash ./tasks/compile-components.sh | uglifyjs -o ./build/lib.js & \
+bash ./tasks/compile-scripts.sh | ng-annotate -a - | uglifyjs -o ./build/index.js & \
 npm run compile-templates &
 
 wait
