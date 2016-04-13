371195230a19cd43c047cebb15f25105aecb6de2
dmi3y <lapshukov@gmail.com>
2016-04-13 12:35:06 -0700

    removing annotate from components

--- tasks/optimize.sh
+++ tasks/optimize.sh
@@ -2,7 +2,7 @@
 
 npm run compile-styles -- -u cssnano & \
 ./tasks/compile-scripts.sh | ng-annotate -a - | uglifyjs -o ./build/index.js & \
-./tasks/compile-components.sh | ng-annotate -a - | uglifyjs -o ./build/lib.js & \
+./tasks/compile-components.sh | uglifyjs -o ./build/lib.js & \
 npm run compile-templates &
 
 wait
