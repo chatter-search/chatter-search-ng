597a7d16987cc43a4f5bdc76ffe6d2fa5f6d6b51
dmi3y <lapshukov@gmail.com>
2016-04-12 18:25:58 -0700

    cleanups

--- tasks/compile-styles.sh
+++ tasks/compile-styles.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash -x
 
+mkdir -p build
+
 node-sass ./src/styles/index.scss \
 | postcss -c ./postcss-options.json \
 -o ./build/index.css
--- tasks/compile-templates.sh
+++ tasks/compile-templates.sh
@@ -2,7 +2,7 @@
 
 version=${npm_package_version-$NPM_PACKAGE_VERSION}
 mkdir -p ./build/templates
-echo $version
+
 cat ./src/index.html | \
 sed -E "s/(<meta name=\"version\" content=\")[[:alnum:].]*(\">)/\1$version\2/" > \
 ./build/index.html
