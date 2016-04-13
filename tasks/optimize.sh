a52786e2be157d63d898d716f9222df36c542d39
dmi3y <lapshukov@gmail.com>
2016-04-13 14:07:34 -0700

    poking around optimize

--- tasks/optimize.sh
+++ tasks/optimize.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash -e
 
 npm run compile-styles -- -u cssnano & \
-./tasks/compile-scripts.sh | ng-annotate -a - | uglifyjs -o ./build/index.js & \
 ./tasks/compile-components.sh | uglifyjs -o ./build/lib.js & \
+./tasks/compile-scripts.sh | ng-annotate -a - | uglifyjs -o ./build/index.js & \
 npm run compile-templates &
 
 wait
