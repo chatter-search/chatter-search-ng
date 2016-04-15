012980daa721e51f34344ca466cced7a8d8ca24d
dmi3y <lapshukov@gmail.com>
2016-04-15 13:18:29 -0700

    bash syntax correction

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash -x
 
-if [ ${CI_BUILD_TAG+x} ]; then npm run optimize && npm run surge; else echo 'Not a tag commit, will not deploy.' fi
+if [ ${CI_BUILD_TAG+x} ]; then npm run optimize && npm run surge; else echo 'Not a tag commit, will not deploy.'; fi
