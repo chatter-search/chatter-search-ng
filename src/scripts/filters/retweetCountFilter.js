c9f02c23411ac6ab172406dfbf494724132d1f24
dmi3y <lapshukov@gmail.com>
2016-04-22 12:20:23 -0700

    allright

--- src/scripts/filters/retweetCountFilter.js
+++ src/scripts/filters/retweetCountFilter.js
@@ -1,15 +1,12 @@
 /* global angular */
 angular.module('ChatterApp')
 .filter('retweetCountFilter', ($filter) => {
-  return (twits, filter) => {
-    if (twits && filter) {
-      if (filter.retweetCount) {
-        twits = $filter('filter')(twits, (el, ix) => {
-          return el.retweet_count > filter.retweetCount
-        })
-      }
-      filter.isNothingFound = !twits.length
-      return twits
+  return (twits, retweetCount) => {
+    if (retweetCount) {
+      twits = $filter('filter')(twits, (el, ix) => {
+        return el.retweet_count > retweetCount
+      })
     }
+    return twits
   }
 })
