d6d5fa5128a6209541ad57a06ccb11726334c644
dmi3y <lapshukov@gmail.com>
2016-04-30 22:24:40 -0700

    order direction functionality

--- src/scripts/controllers/TwitsToolbarController.js
+++ src/scripts/controllers/TwitsToolbarController.js
@@ -2,8 +2,10 @@
 angular.module('ChatterApp')
 .controller('TwitsToolbarController', ($scope, $filter) => {
   $scope.sortBy = 'created_at_timestamp'
+  $scope.sortByDir = -1
   $scope.filter = {
     retweetCount: 0,
     hasImage: false
   }
+  $scope.isDecending = () => { return $scope.sortByDir === -1 }
 })
