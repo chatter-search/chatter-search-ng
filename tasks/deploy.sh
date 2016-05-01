ad7f1fe58de954be683e68dd8e0a1ff0cb2cc860
dmi3y <lapshukov@gmail.com>
2016-04-30 23:03:51 -0700

    formatting deploy script

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash -x
 
-if [ ${CI_BUILD_TAG+x} ]; then npm run optimize && npm run surge; else echo 'Not a tag commit, will not deploy.'; fi
+if [ ${CI_BUILD_TAG+x} ];
+    then npm run optimize && npm run surge;
+    else echo 'Not a tag commit, will not deploy.';
+fi
